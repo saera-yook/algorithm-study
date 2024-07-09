25df4143b7a0f6d69cd2629db6d2d486dc49b38c
Saera Yook <148451132+saera-yook@users.noreply.github.com>
2024-07-09 16:14:31 +0900

    [Bronze V] Title: A+B - 4, Time: 168 ms, Memory: 16476 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        while (input != null) {
+            int[] numbers = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
+            bw.write(numbers[0] + numbers[1] + System.lineSeparator());
+            input = br.readLine();
+        }
+        bw.close();
+    }
+}
