7c002aa58d3a66ca05629faa60ac86470f0651bb
Saera Yook <148451132+saera-yook@users.noreply.github.com>
2024-07-10 17:26:43 +0900

    [Bronze V] Title: 문자와 문자열, Time: 108 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        int offset = Integer.parseInt(br.readLine()) - 1;
+        bw.write(input, offset, 1);
+        bw.close();
+    }
+}
