b09291a5f54d85a2bba751062c87d02db7eefd6b
Saera Yook <148451132+saera-yook@users.noreply.github.com>
2024-07-14 14:39:00 +0900

    [Bronze III] Title: 최소, 최대, Time: 484 ms, Memory: 112204 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,20 +1,27 @@
-import static java.util.Arrays.stream;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        br.readLine();
-        int[] numbers = stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int min = stream(numbers).min().orElseThrow();
-        int max = stream(numbers).max().orElseThrow();
-        bw.write(String.valueOf(min) + " " + String.valueOf(max));
+        int count = Integer.parseInt(br.readLine());
+        int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int min = numbers[0];
+        int max = numbers[count - 1];
+        for (int i = 0; i < count; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        bw.write(min + " " + max);
         bw.close();
     }
 }
