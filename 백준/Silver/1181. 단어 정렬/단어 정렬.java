1d5ce41f5f60d0d14b04711f27053401a23755f3
Saera Yook <148451132+saera-yook@users.noreply.github.com>
2024-07-24 16:29:06 +0900

    [Silver V] Title: 단어 정렬, Time: 1888 ms, Memory: 26892 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        List<String> words = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            String word = br.readLine();
+            if (!words.contains(word)) {
+                words.add(word);
+            }
+        }
+        words.sort(Comparator.naturalOrder());
+        words.sort(Comparator.comparing(String::length));
+        for (String word : words) {
+            sb.append(word).append(System.lineSeparator());
+        }
+        bw.write(sb.toString());
+        bw.close();
+    }
+}
