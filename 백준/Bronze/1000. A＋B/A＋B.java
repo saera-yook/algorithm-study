e4eb707f7c88e01bd05a23fec6e2e9a0458a3d81
Saera Yook <148451132+saera-yook@users.noreply.github.com>
2024-07-25 23:52:26 +0900

    [Bronze V] Title: A+B, Time: 132 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] input = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        bw.write(String.valueOf(a + b));
+        bw.flush();
+    }
+}
