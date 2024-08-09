c6e2c8f42b6bde53a1d534126060670183761092
Saera Yook <148451132+saera-yook@users.noreply.github.com>
2024-08-09 22:22:38 +0900

    [Silver IV] Title: 큐, Time: 164 ms, Memory: 20336 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -23,28 +23,33 @@ public class Main {
         for (int i = 0; i < N; i++) {
             stk = new StringTokenizer(br.readLine());
             String command = stk.nextToken();
-            if (stk.hasMoreTokens()) {
-                queue.offer(Integer.parseInt(stk.nextToken()));
-            } else if (EMPTY.equals(command)) {
-                if (queue.isEmpty()) {
-                    sb.append(1).append(System.lineSeparator());
-                } else {
-                    sb.append(0).append(System.lineSeparator());
-                }
-            } else if (SIZE.equals(command)) {
-                sb.append(queue.size()).append(System.lineSeparator());
-            } else {
-                try {
-                    if (POP.equals(command)) {
+            try {
+                switch (command) {
+                    case "push":
+                        queue.offer(Integer.parseInt(stk.nextToken()));
+                        break;
+                    case EMPTY:
+                        if (queue.isEmpty()) {
+                            sb.append(1).append(System.lineSeparator());
+                        } else {
+                            sb.append(0).append(System.lineSeparator());
+                        }
+                        break;
+                    case SIZE:
+                        sb.append(queue.size()).append(System.lineSeparator());
+                        break;
+                    case POP:
                         sb.append(queue.removeFirst()).append(System.lineSeparator());
-                    } else if (FRONT.equals(command)) {
+                        break;
+                    case FRONT:
                         sb.append(queue.getFirst()).append(System.lineSeparator());
-                    } else {
+                        break;
+                    default:
                         sb.append(queue.getLast()).append(System.lineSeparator());
-                    }
-                } catch (NoSuchElementException e) {
-                    sb.append(-1).append(System.lineSeparator());
+                        break;
                 }
+            } catch (NoSuchElementException e) {
+                sb.append(-1).append(System.lineSeparator());
             }
         }
         br.close();
