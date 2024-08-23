66681c8c005d55c9284bf09cc1fc099ad356ffe0
Saera Yook <148451132+saera-yook@users.noreply.github.com>
2024-08-23 12:08:34 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -3,35 +3,40 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    private static int maxN = 40;
-    private static int[] countOf0 = new int[maxN + 1];
-    private static int[] countOf1 = new int[maxN + 1];
+    private static final int maxN = 40;
+    private static final List<Integer> countOf0 = new ArrayList<>();
+    private static final List<Integer> countOf1 = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+        countOf0.add(0, 1);
+        countOf1.add(0, 0);
+        countOf0.add(1, 0);
+        countOf1.add(1, 1);
+        for (int i = 2; i <= maxN; i++) {
+            countOf0.add(i, null);
+            countOf1.add(i, null);
+        }
         int T = Integer.parseInt(br.readLine());
-        calculateCounts(maxN);
         for (int i = 0; i < T; i++) {
             int N = Integer.parseInt(br.readLine());
-            sb.append(countOf0[N] + " ").append(countOf1[N] + "\n");
+            sb.append(calculateCounts(countOf0, N)).append(" ").append(calculateCounts(countOf1, N)).append("\n");
         }
         br.close();
         bw.write(sb.toString());
         bw.close();
     }
-
-    private static void calculateCounts(int n) {
-        countOf0[0] = 1;
-        countOf1[0] = 0;
-        countOf0[1] = 0;
-        countOf1[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            countOf0[i] = countOf0[i - 1] + countOf0[i - 2];
-            countOf1[i] = countOf1[i - 1] + countOf1[i - 2];
+    
+    private static int calculateCounts(List<Integer> list, int n) {
+        if (list.get(n) == null) {
+            list.set(n, calculateCounts(list, n - 1) + calculateCounts(list, n - 2));
         }
+        return list.get(n);
     }
 }
