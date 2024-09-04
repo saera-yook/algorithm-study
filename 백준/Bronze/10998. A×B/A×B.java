64efd5818c3a0f3c56a7bb533904ad2ee07adfc8
Saera Yook <148451132+saera-yook@users.noreply.github.com>
2024-09-04 23:10:45 +0900

    [Bronze V] Title: A×B, Time: 96 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(stk.nextToken());
+        int B = Integer.parseInt(stk.nextToken());
+        bw.write(String.valueOf(A * B));
+        br.close();
+        bw.close();
+    }
+}
