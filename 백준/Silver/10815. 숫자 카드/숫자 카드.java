7bc0cc6526b57087aa821cfbd8f95d94177885ec
Saera Yook <148451132+saera-yook@users.noreply.github.com>
2024-09-13 20:54:21 +0900

    [Silver V] Title: 숫자 카드, Time: 848 ms, Memory: 136872 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -3,32 +3,25 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static List<Integer> cards;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         StringTokenizer stk = new StringTokenizer(br.readLine());
-        cards = new ArrayList<>();
+        Set<Integer> cards = new HashSet<>();
         for (int i = 0; i < N; i++) {
             cards.add(Integer.parseInt(stk.nextToken()));
         }
-        cards.sort(Comparator.naturalOrder());
         int M = Integer.parseInt(br.readLine());
         stk = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            int low = 0;
-            int high = N - 1;
-            int testNumber = Integer.parseInt(stk.nextToken());
-            if (isExist(testNumber, low, high)) {
+            if (cards.contains(Integer.parseInt(stk.nextToken()))) {
                 sb.append(1).append(" ");
             } else {
                 sb.append(0).append(" ");
@@ -37,20 +30,5 @@ public class Main {
         bw.write(sb.toString());
         br.close();
         bw.close();
-    }
-
-    private static boolean isExist(int n, int low, int high) {
-        if (low > high) {
-            return false;
-        }
-        int mid = (int) Math.floor((low + high) / 2.0);
-        int existNumber = cards.get(mid);
-        if (n < existNumber) {
-            return isExist(n, low, mid - 1);
-        } else if (n > existNumber){
-            return isExist(n, mid + 1, high);
-        } else {
-            return true;
-        }
     }
 }
